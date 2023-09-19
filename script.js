ff23dbc4cd9c3bee1aab858a119f003fb624ce8b
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-09-20 00:41:31 +0300

    My-Cart

---
+++ script.js
@@ -0,0 +1,44 @@
+const btn1 = document.querySelector(".decrease")
+const btn2 = document.querySelector(".increase")
+const count = document.querySelector(".one")
+const mainCount = parseInt(count.textContent)
+
+    for( let i = 1 ; i <= 9 ; i++){
+
+
+        let counter = 0
+    
+       btn1.addEventListener("click" , ()=>{
+            counter--;
+            count.textContent = counter ;
+            if(count.textContent >= 9){
+                count.textContent = 9
+        
+            } else if (count.textContent <=0 ){
+                count.textContent = 1
+            }
+    
+        })
+        btn2.addEventListener("click" , () =>{
+            counter++;
+            count.textContent = counter ;
+            if(count.textContent >= 9){
+                count.textContent = 9
+        
+            } else if (count.textContent <=0 ){
+                count.textContent = 1
+            }
+    
+        })
+        
+    }
+   
+
+
+
+    
+
+    
+
+ 
+
