bcbc153eda2d453fded2e0781992a61aeaa079de
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-09-22 04:17:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,29 @@ const count = document.querySelector(".one")
 
 for( let i = 1 ; i <= 9 ; i++){
 
-
     let counter = 0
-
+    
    btn1.addEventListener("click" , ()=>{
         counter--;
         count.textContent = counter ;
-        if(count.textContent >= 9){
+        const amount = parseInt(count.textContent)
+        if(amount >= 9){
             count.textContent = 9
-        } else if (count.textContent <=0 ){
+        } else if (amount <= 0 ){
             count.textContent = 1
         }
     })
     btn2.addEventListener("click" , () =>{
         counter++;
         count.textContent = counter ;
-        if(count.textContent >= 9){
+        const amount = parseInt(count.textContent)
+
+        if(amount >= 9){
             count.textContent = 9
-        } else if (count.textContent <=0 ){
+        } else if (amount <= 0 ){
             count.textContent = 1
         }
     })
 }
+
+    
